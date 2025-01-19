fdd9a30b2d9307a39d1112e58dd8ae88535a8180
Pablo <116576629+Ppm19@users.noreply.github.com>
2025-01-19 16:28:26 +0100

    Update Alumno.js

--- models/Alumno.js
+++ models/Alumno.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('../db');
 
 const AlumnoSchema = new mongoose.Schema({
@@ -9,15 +8,3 @@ const AlumnoSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Alumno', AlumnoSchema);
-=======
-const mongoose = require('../db');
-
-const AlumnoSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  nombre: { type: String, required: true },
-  apellido: { type: String, required: true },
-  telefono: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Alumno', AlumnoSchema);
->>>>>>> 171e33f15144b6cf407379edf6c9749453bee35e
